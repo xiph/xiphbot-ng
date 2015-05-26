8c46c284bc602911f0e08a8514ce0ff5f7ec137c
Thomas Daede <daede003@umn.edu>
2015-05-26 15:00:14 -0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+extern crate irc;
+
+use irc::client::prelude::*;
+use std::net::UdpSocket;
+use std::sync::Arc;
+
+fn main() {
+    let socket = UdpSocket::bind("127.0.0.1:34254").unwrap();
+    let server = Arc::new(IrcServer::new("config.json").unwrap());
+    server.identify().unwrap();
+    let server_udp_thread = server.clone();
+    std::thread::spawn(move || {
+        let mut buf = [0; 1000];
+        loop {
+            let _ = socket.recv_from(&mut buf);
+            let s = std::str::from_utf8(&mut buf).unwrap();
+            println!("{}", &s);
+            let _ = server_udp_thread.send(Command::PRIVMSG("#xiph".to_string(), s.to_string()));
+        }
+    });
+    for _ in server.iter() {
+        // nothing to handle
+    }
+}
